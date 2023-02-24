d268208f4dcda4a7a4ef8abec3eafbd46c7950b9
Francesco <G00425679@atu.ie>
2023-02-24 17:11:20 +0000

    collatz countdown

---
+++ collatz.py
@@ -0,0 +1,18 @@
+#inpt a positive int ad output the following:
+#if the value is even, divide it by two
+#if the value is odd, multiply it by 3 and add 1
+
+#Author: Franesco Troja
+
+value = int(input("Please enter a positive integer: "))
+
+
+while value != 1:
+    if (value % 2) == 0:
+            value = value // 2
+            print(value, end= " ") #adding end prameter in order to avoid the creation of a new line between int
+    else:
+        (value % 2) != 0
+        value = (value * 3 + 1)
+        print(value, end= " ")
+
