3f1e8ce119617965690c96c3f24ca2f868491e1a
Francesco <G00425679@atu.ie>
2023-02-24 17:13:10 +0000

    Collatz countdown

--- collatz.py
+++ collatz.py
@@ -10,7 +10,7 @@ value = int(input("Please enter a positive integer: "))
 while value != 1:
     if (value % 2) == 0:
             value = value // 2
-            print(value, end= " ") #adding end prameter in order to avoid the creation of a new line between int
+            print(value, end= " ") #adding end prameter in order to avoid the creation of a new line between the different int
     else:
         (value % 2) != 0
         value = (value * 3 + 1)
