3d243602b0cee9f332a5fe21e71798e557d3c01a
Francesco <G00425679@atu.ie>
2023-03-18 12:05:22 +0000

    Count the E in the text

--- readTextFile.py
+++ readTextFile.py
@@ -3,12 +3,24 @@
 #Author: Francesco Troja
 
 
-def letter_frequency(FILENAME, letter):
-    with open(FILENAME, "r") as f:
-        doc = f.read()
-        return doc.count("e") #the esiest way to find the number of E in the text was to use the count() method
+import sys #the sys module is used by the user to pass a command-line arguments in the script
 
-print(letter_frequency("text.txt", "e"))
+if len(sys.argv) < 2: 
+    print("Error, please add a <filepath>")
+    sys.exit(1)
+
+filepath = sys.argv[1] #we use sys.argv[1] as sys.argv[0] will always give us the name of the script.
+
+with open(filepath, "r") as f:
+    text = f.read()
+
+count = 0
+for char in text:
+    if char == "e" or char == "E":
+        count += 1
+#in the previous script I took into consideration to count only the lowercase 'e'. To give more accuracy i added also the chance to count the upper case 'E'.
+
+print(count)
 
 
 
