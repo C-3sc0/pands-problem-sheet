c64b3e593be9a3b9fcb4fc2fcf7a2b675f0c2cf6
Francesco <G00425679@atu.ie>
2023-03-25 18:46:40 +0000

    My cents in the bank account

--- bank.py
+++ bank.py
@@ -1,14 +1,14 @@
-#Program should prompt the user and read in two
-#money amounts (in cents).
-#Print out the answer with Euro sign and decimal point
-#beteen the euro and cent of the amount
+#Program should prompt the user and read in two money amounts (in cents).
+#Print out the answer with Euro sign and decimal point beteen the euro and cent of the amount
 
 #Author: Francesco Troja
 
-amount = int(input("Enter amount1: "))
-amount1 = int(input("Enter amount2: "))
-total = (amount + amount1)/100
+def bank():
+    amount = int(input("Enter amount1: "))
+    amount1 = int(input("Enter amount2: "))
+    total = (amount + amount1)/100 
+    return total
 
-#to retrieve the € symbol i used its Unicode glyph number
-
-print (f'The sum of these is \u20ac{total}')
+sum = bank()
+#to retrieve the € symbol, I used its Unicode glyph number
+print (f'The sum of these is \u20ac{sum}')
