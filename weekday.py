db7db5a3c4f121df7741f8d8b8e433fbd4c7d9cc
Francesco <G00425679@atu.ie>
2023-04-17 23:18:45 +0100

    What day is today?

--- weekday.py
+++ weekday.py
@@ -5,7 +5,7 @@
 from datetime import datetime
 today = datetime.today().isoweekday()
 
-if today < 5:
+if today < 5: #since isoweekday method returns the day of the week as int, where Monday is 1 and Sunday 7, if the current day is between Monday and Thursday the first statement will be printed  
     print("Yes, unfortunately today is a weekday.")
 else:
     print("It is the weekend, yay!")
